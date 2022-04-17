f3fc03f43356156545271fce1c8f2e23f61e1e84
Stilan <40999153+Stilan@users.noreply.github.com>
2022-04-17 21:29:40 +0700

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/forum/control/LoginControlTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.forum.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.Main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class LoginControlTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @WithMockUser
+    public void loginPageTest() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+}
